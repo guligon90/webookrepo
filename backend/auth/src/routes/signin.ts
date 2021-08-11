236805e30a8e49d4596f9cdc12484876ce002550
Guilherme Lima Gonçalves <guligon90@gmail.com>
2021-08-11 05:10:19 -0300

    refactor(auth): substitute old errors and middlewares imports by the new common package

--- backend/auth/src/routes/signin.ts
+++ backend/auth/src/routes/signin.ts
@@ -2,10 +2,9 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
 
-import { BadRequestError } from '../errors/bad-request-error';
 import { PasswordHandler } from '../services/password';
 import { User } from '../models/user';
-import { validateRequest } from '../middlewares/validate-request';
+import { BadRequestError, validateRequest } from '@webookrepo/common';
 
 const router = express.Router();
 
